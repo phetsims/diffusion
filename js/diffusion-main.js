470522d6c5ffd823a21e1db0185dfbf420202e69
pixelzoom <cmalley@pixelzoom.com>
2020-11-09 21:16:55 -0700

    convert to Sprites, https://github.com/phetsims/gas-properties/issues/157

--- js/diffusion-main.js
+++ js/diffusion-main.js
@@ -11,10 +11,14 @@ import GasPropertiesGlobalOptionsNode from '../../gas-properties/js/common/view/
 import DiffusionScreen from '../../gas-properties/js/diffusion/DiffusionScreen.js';
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+import Utils from '../../scenery/js/util/Utils.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import diffusionStrings from './diffusionStrings.js';
 
 const simOptions = {
+
+  // Enabled for high-performance Sprites
+  webgl: true,
 
   // Creates content for the Options dialog, accessible via the PhET menu
   createOptionsDialogContent: tandem => new GasPropertiesGlobalOptionsNode( {
@@ -29,8 +33,13 @@ const simOptions = {
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
+
   const sim = new Sim( diffusionStrings.diffusion.title, [
     new DiffusionScreen( Tandem.ROOT.createTandem( 'diffusionScreen' ) )
   ], simOptions );
+
+  // Log whether we're using WebGL, which is the preferred rendering option for Sprites
+  phet.log && phet.log( `using WebGL = ${phet.chipper.queryParameters.webgl && Utils.isWebGLSupported}` );
+
   sim.start();
 } );

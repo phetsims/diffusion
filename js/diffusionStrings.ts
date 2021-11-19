ef2d1421fad683b6bf79343ffb0b925665101e25
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:08 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/diffusionStrings.ts
+++ js/diffusionStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import diffusion from './diffusion.js';
 
-const diffusionStrings = getStringModule( 'DIFFUSION' );
+type StringsType = {
+  'diffusion': {
+    'title': string
+  }
+};
+
+const diffusionStrings = getStringModule( 'DIFFUSION' ) as StringsType;
 
 diffusion.register( 'diffusionStrings', diffusionStrings );
 

2211db17785d748558af933cca0e734703a314d1
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/diffusionStrings.ts
+++ js/diffusionStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import diffusion from './diffusion.js';
 
 type StringsType = {
   'diffusion': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 

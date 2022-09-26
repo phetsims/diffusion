2bce748b1f8db8d1950e66bfc3b268f62fb03c65
Sam Reid <reids@colorado.edu>
2022-09-26 12:16:20 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/DiffusionStrings.ts
+++ js/DiffusionStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import diffusion from './diffusion.js';
 
 type StringsType = {
   'diffusion': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 

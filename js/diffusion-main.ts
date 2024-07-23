74162b1ae22c58699a1d8361d439d7cbdfd385c6
pixelzoom <cmalley@pixelzoom.com>
2024-07-23 14:42:14 -0600

    factored out SimOptions, set webgl:false for mobile safari, https://github.com/phetsims/gas-properties/issues/289

--- js/diffusion-main.ts
+++ js/diffusion-main.ts
@@ -9,28 +9,30 @@
 import GasPropertiesConstants from '../../gas-properties/js/common/GasPropertiesConstants.js';
 import DiffusionScreen from '../../gas-properties/js/diffusion/DiffusionScreen.js';
 import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
-import Sim from '../../joist/js/Sim.js';
+import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import { Utils } from '../../scenery/js/imports.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import DiffusionStrings from './DiffusionStrings.js';
+import { combineOptions } from '../../phet-core/js/optionize.js';
 
 simLauncher.launch( () => {
+
+  const titleStringProperty = DiffusionStrings.diffusion.titleStringProperty;
 
   const screens = [
     new DiffusionScreen( Tandem.ROOT.createTandem( 'diffusionScreen' ) )
   ];
 
-  const sim = new Sim( DiffusionStrings.diffusion.titleStringProperty, screens, {
-    phetioDesigned: true,
-    webgl: true, // Enabled for high-performance Sprites
-    credits: GasPropertiesConstants.CREDITS,
+  const options = combineOptions<SimOptions>( {}, GasPropertiesConstants.SIM_OPTIONS, {
     preferencesModel: new PreferencesModel( {
       visualOptions: {
         supportsProjectorMode: true
       }
     } )
   } );
+
+  const sim = new Sim( titleStringProperty, screens, options );
 
   // Log whether we're using WebGL, which is the preferred rendering option for Sprites
   phet.log && phet.log( `using WebGL = ${phet.chipper.queryParameters.webgl && Utils.isWebGLSupported}` );
